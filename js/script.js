f5fe8fee9750dd100f0b71bc034db5a1336c942e
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-24 17:04:51 +0100

    Aggiornamento (la lista con un buttone)

--- js/script.js
+++ js/script.js
@@ -3,17 +3,20 @@ const app = new Vue({
     date: {
     },
     methods: {
-      
-    },
-    mounted() {
+      emailList(){
+        document.querySelector('ol').innerHTML = '';
+        document.getElementById('image').innerHTML = '';
         for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(function (response){
                 const result = response.data.response;
-                console.log(result);
                 document.querySelector('ol').innerHTML += `<li>${result}</li>`;
             });
         }
-        
+      },
+      image(){
+        document.querySelector('ol').innerHTML = '';
+        document.getElementById('image').innerHTML = `<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkf662_C-fcZbWA2jyoA4E0xK_BwgSDKA33A&usqp=CAU">`
+      }
     }
 })
